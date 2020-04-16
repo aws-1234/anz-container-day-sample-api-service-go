b03b843a932dbad141ba43991e1402a899252c25
aws-1234 <63759708+aws-1234@users.noreply.github.com>
2020-04-16 12:50:49 +1200

    Here's a commit message
    didn't do anything!

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello Richard - the log message")
+		fmt.Println("Hello Richard - from the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
